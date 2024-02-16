699395fa5024505497ea4b49d45ce285757e3da0
ViniMR11 <mantovaniv232@gmail.com>
2024-02-16 11:07:33 -0300

    Etapa 01

---
+++ 02_EstruturaDeDecisão/Exercicio3.dart
@@ -0,0 +1,15 @@
+void main() {
+ 
+ const nota1 = 5;
+ const nota2 = 6;
+ const nota3 = 8;
+ const nota4 = 7; 
+  
+ 
+  double media = 5;
+  if (media>= 5) {
+    print(nota4);
+  } else {
+    print(nota3);
+  }
+}
